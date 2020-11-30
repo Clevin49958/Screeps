444fd6364742a9048c85612dd3847da26a9e08d3
Kevin Hu <hyxkv925@gmail.com>
2020-12-01 01:07:12 +1300

    Use quest logic for claimer

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -3,26 +3,27 @@ const helper = require("./helper");
 module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
+        if (!creep.memory.quest && Game.rooms[creep.memory.target]){
+            creep.memory.quest = Game.rooms[creep.memory.target].controller.id;
+        }
 
-        if (creep.room.name == creep.memory.target) {
-            if (creep.room.controller) {
-                var controller = creep.room.controller;
-                if (creep.pos.isNearTo(creep.room.controller)){
-                    if (!creep.room.controller.my && creep.room.controller
-                        .owner) {
-                        creep.attackController(creep.room.controller)
-                    } else {
-                        if (creep.claimController(creep.room.controller) == ERR_GCL_NOT_ENOUGH){
-                            creep.reserveController(creep.room.controller);
-                        }
-                    }
+        if (!creep.memory.quest && !creep.room.name == creep.memory.target) {
+            return helper.moveTargetRoom(creep);
+        }
+        if (creep.memory.quest){
+            var controller = Game.getObjectById(creep.memory.quest);
+            if (creep.pos.isNearTo(controller)){
+                if (!controller.my && controller
+                    .owner) {
+                    creep.attackController(controller)
                 } else {
-                    creep.myMoveTo(creep.room.controller);
+                    if (creep.claimController(controller) == ERR_GCL_NOT_ENOUGH){
+                        creep.reserveController(controller);
+                    }
                 }
-                
+            } else {
+                creep.myMoveTo(controller);
             }
-        } else {
-            helper.moveTargetRoom(creep);
         }
     }
 };
