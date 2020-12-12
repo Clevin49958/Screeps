7170bb4e4d16ccd43cec8d484190d00f82bfe069
Kevin Hu <hyxkv925@gmail.com>
2020-12-12 22:21:32 +1300

    Miner is smarter and pay terminal as well now

--- src/role.miner.js
+++ src/role.miner.js
@@ -2,43 +2,60 @@ const helper = require('./helper');
 const {Logger} = require('./Logger');
 
 module.exports = {
-  // a function to run the logic for this role
+  /**
+   * Miner will harvest mineral from creep.memory.target (room), and store in a terminal if in reach
+   * @param {Creep} creep 
+   */
   run: function(creep) {
-    if (!creep.memory.source) {
-      if (Game.rooms[creep.memory.target]) {
-        creep.memory.source = Game.rooms[creep.memory.target].find(FIND_MINERALS)[0].id;
-      } else {
-        return helper.moveTargetRoom(creep);
+    if (Game.rooms[creep.memory.target]) {
+      if (!creep.memory.source) {
+        if (Game.rooms[creep.memory.target]) {
+          const mineral = Game.rooms[creep.memory.target].find(FIND_MINERALS)[0];
+          creep.memory.source = mineral.id;
+          creep.memory.mineralType = mineral.mineralType;
+        } else {
+          return helper.moveTargetRoom(creep);
+        }
+      }
+      if (!creep.memory.extractor) {
+        const extractors = Game.rooms[creep.memory.target].find(
+            FIND_STRUCTURES, {filter: (s) =>
+              s.structureType == STRUCTURE_EXTRACTOR,
+            });
+        if (extractors.length) {
+          creep.memory.extractor = extractors[0].id;
+        } else {
+          Logger.warn(creep.memory.target, 'no extractor found');
+          return;
+        }
       }
     }
-    if (!creep.memory.extractor) {
-      const extractors = Game.rooms[creep.memory.target].find(
-          FIND_STRUCTURES, {filter: (s) =>
-            s.structureType == STRUCTURE_EXTRACTOR,
-          });
-      if (extractors.length) {
-        creep.memory.extractor = extractors[0].id;
-      } else {
-        Logger.warn(creep.memory.target, 'no extractor found');
-        return;
-      }
-    }
+    
     // console.log(creep.name,JSON.stringify(Game.getObjectById(creep.memory.source)))
     const source = Game.getObjectById(creep.memory.source);
     const extractor = Game.getObjectById(creep.memory.extractor);
+    const terminal = Game.getObjectById(creep.memory.terminal);
     // if arrived
     if (creep.memory.arrived == true) {
-      // check for presence of link
-      if (creep.memory.link === undefined) {
-        const links = creep.pos.findInRange(FIND_STRUCTURES,
-            1, {filter: (s) => s.structureType == STRUCTURE_LINK});
-        creep.memory.link = (links.length > 0) ? links[0].id : null;
-      }
       creep.say(extractor.cooldown);
+      if (!creep.memory.terminal) {
+        const terminals = creep.pos.findInRange(
+            FIND_STRUCTURES, 1, {filter: (s) =>
+              s.structureType == STRUCTURE_TERMINAL,
+            });
+        if (terminals.length) {
+          creep.memory.terminal = terminals[0].id;
+          terminal = terminals[0];
+        } 
+      }
       if (source.mineralAmount > 0 && extractor.cooldown === 0) {
         creep.harvest(source);
+      } else {
+        if (terminal && creep.store.getUsedCapacity(creep.memory.mineralType) > 0){
+          creep.transfer(terminal, creep.memory.mineralType);
+        }
       }
-    } else if (creep.pos.isNearTo(source)) {
+    } else if (creep.pos.inRangeTo(source, 2)) {
       let container = creep.pos.findInRange(FIND_STRUCTURES,
           2, {
             filter: (s) => s.structureType ==
@@ -49,8 +66,12 @@ module.exports = {
             FIND_CONSTRUCTION_SITES, 2);
       }
       if (container.length == 0) {
-        creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
-        return;
+        if (creep.pos.isNearTo(source)) {
+          creep.room.createConstructionSite(creep.pos, STRUCTURE_CONTAINER);
+        } else {
+          creep.myMoveTo(source);
+          return true;
+        }
       }
       if (creep.pos.isEqualTo(container[0])) {
         creep.memory.arrived = true;
@@ -59,6 +80,7 @@ module.exports = {
         creep.myMoveTo(container[0]);
       }
     } else {
+      // TODO: error will occur when creep has no vision to source
       creep.myMoveTo(source);
     }
   },
