3879ddef6380e61327f5bd5e7db66a108f930760
Kevin Hu <hyxkv925@gmail.com>
2020-12-22 22:07:24 +1300

    Sort terminal orders to sell to highest price deal

--- src/terminal.js
+++ src/terminal.js
@@ -1,3 +1,4 @@
+const { Logger } = require("./Logger");
 
 /**
  * this will attempt to sell any resource that is in excess
@@ -6,7 +7,7 @@
 function autoDealExcess(terminal) {
     const THRESHOLD = 100000;
     const MAX_PER_SELL = 10000;
-    const TRANSITION_COST_RATIO = 0.75;
+    const TRANSITION_COST_RATIO = 1;
 
 
     if (terminal.cooldown > 0) {
@@ -25,19 +26,28 @@ function autoDealExcess(terminal) {
         return OK;
     }
 
-    const orders = Game.market.getAllOrders({type: ORDER_SELL, resourceType: srcType});
+    const orders = Game.market.getAllOrders({type: ORDER_BUY, resourceType: resourceType}).sort((a,b) => b.price - a.price);
+    const minPrice = _.max([orders[0].price * 0.7, _.sum(orders.map(o => o.price)) / orders.length])
     for (const order of orders) {
-        const amountToBuy = _.max([MAX_PER_SELL, order.amount]);
+        const amountToBuy = _.min([MAX_PER_SELL, order.amount]);
         const transferEnergyCost = Game.market.calcTransactionCost(
             amountToBuy, terminal.room.name, order.roomName);
-    
-        if(transferEnergyCost / amountToBuy < TRANSITION_COST_RATIO) {
-            return Game.market.deal(order.id, amountToBuy, terminal.room.name);
+        if(transferEnergyCost / amountToBuy < TRANSITION_COST_RATIO && order.price > minPrice) {
+            if (terminal.store.energy < transferEnergyCost) {
+                return ERR_NOT_ENOUGH_ENERGY;
+            }
+            const res = Game.market.deal(order.id, amountToBuy, terminal.room.name);
+            const msg = `Sell ${order.roomName} for ${order.resourceType} @${amountToBuy}x$${order.price} res ${res}`;
+            Logger.info(msg);
+            Game.notify(msg);
+            return res;
         }
     }
+
+    return OK;
 }
 
 module.exports = {
     autoDealExcess,
-    
+
 }
