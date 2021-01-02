43c8f7571def59ba7c3e9fd58258368e18ccc28e
Kevin Hu <hyxkv925@gmail.com>
2021-01-02 15:17:27 +1300

    initial model of task queue

---
+++ src/priorityQueue.js
@@ -0,0 +1,107 @@
+class queueNode {
+  constructor(priority) {
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.elements = [];
+  }
+
+  // helper method that swaps the elements and two indexes of an array
+  swap(index1, index2) {
+    const temp = this.elements[index1];
+    this.elements[index1] = this.elements[index2];
+    this.elements[index2] = temp;
+    return this.elements;
+  }
+  // helper methods that bubbles up elements from end
+  bubbleUp() {
+    // get index of inserted element
+    let index = this.elements.length - 1;
+    // loop while index is not 0 or element no loger needs to bubble
+    while (index > 0) {
+      // get parent index via formula
+      const parentIndex = Math.floor((index - 1) / 2);
+      // if elements is greater than parent, swap the two
+      if (this.elements[parentIndex].priority > this.elements[index].priority) {
+        // swap with helper method
+        this.swap(index, parentIndex);
+        // change current index to parent index
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+    return 0;
+  }
+  // method that pushes new value onto the end and calls the bubble helper
+  enqueue(value) {
+    this.elements.push(value);
+    // calculate parent, if parent is greater swap
+    // while loop or recurse
+    this.bubbleUp();
+    return this.elements;
+  }
+
+  bubbleDown() {
+    let parentIndex = 0;
+    const length = this.elements.length;
+    const elementPriority = this.elements[0].priority;
+    // loop breaks if no swaps are needed
+    while (true) {
+      // get indexes of child elements by following formula
+      const leftChildIndex = (2 * parentIndex) + 1;
+      const rightChildIndex = (2 * parentIndex) + 2;
+      let leftChildPriority; let rightChildPriority;
+      let indexToSwap = null;
+      // if left child exists, and is greater than the element, plan to swap with the left child index
+      if (leftChildIndex < length) {
+        leftChildPriority = this.elements[leftChildIndex].priority;
+        if (leftChildPriority < elementPriority) {
+          indexToSwap = leftChildIndex;
+        }
+      }
+      // if right child exists
+      if (rightChildIndex < length) {
+        rightChildPriority = this.elements[rightChildIndex].priority;
+
+        if (
+        // if right child is greater than element and there are no plans to swap
+          (rightChildPriority < elementPriority && indexToSwap === null) ||
+                    // OR if right child is greater than left child and there ARE plans to swap
+                    (rightChildPriority < leftChildPriority && indexToSwap !== null)) {
+          // plan to swap with the right child
+          indexToSwap = rightChildIndex;
+        }
+      }
+      // if there are no plans to swap, break out of the loop
+      if (indexToSwap === null) {
+        break;
+      }
+      // swap with planned element
+      this.swap(parentIndex, indexToSwap);
+      // starting index is now index that we swapped with
+      parentIndex = indexToSwap;
+    }
+  }
+
+  dequeue() {
+    // swap first and last element
+    this.swap(0, this.elements.length - 1);
+    // pop max value off of elements
+    const poppedNode = this.elements.pop();
+    // re-adjust heap if length is greater than 1
+    if (this.elements.length > 1) {
+      this.bubbleDown();
+    }
+
+    return poppedNode;
+  }
+}
+
+module.exports = {
+  queueNode,
+  PriorityQueue
+}
--- src/task.js
+++ src/task.js
@@ -1,8 +1,116 @@
+const helper = require('./helper');
+const {queueNode, PriorityQueue} = require('./priorityQueue');
 /**
  * This module aim to provide a list of base classes for task based process
-*/
+ */
 
+class task extends queueNode{
+    defaultAction() {}
+  
 
-class TaskRequest {
+    constructor(priority, aciton = this.defaultAction) {
+      super(priority);
+      /**
+      * 1: complete
+      *
+      * 0~1: in progress (decimal may indicate progress)
+      *
+      * 0: intiated
+      *
+      * -1: failed
+      *
+      * @type {number}
+      */
+      this.status = 0;
+      /**
+         * @function
+         * @param {Creep} creep the creep performing task
+         */
+      this.perform = aciton;
+    }
+}
+class TaskQueue extends PriorityQueue {
+  constructor() {
+    super();
+  }
+}
 
+class payTask extends task {
+  constructor(priority, srcType, amount, target, aciton = this.defaultAction) {
+    this.priority = priority;
+    this.srcType = srcType;
+    this.progressTotal = amount;
+    /** @type {AnyStoreStructure} */
+    this.target = target;
+    this.aciton = aciton;
+    this.progress = 0;
+  }
+
+    /**
+     * creep will pay $amount of $srcType to $target
+     * @param {Creep} creep the creep to perform task
+     * @returns {number} result of task:
+     * 1: complete
+     * 0: OK
+     * <0: one of the ERR_*
+     */
+    defaultAction(creep) {
+      // pay structure
+      const structure = this.target;
+      if (creep.pos.isNearTo(structure)) {
+        const amountCarried = creep.store.getUsedCapacity(this.srcType);
+        const res = creep.transfer(structure, this.srcType);
+        const amountPaid = amountCarried - creep.store.getUsedCapacity(this.srcType);
+        this.progress += amountPaid;
+        this.status = this.progress / this.progressTotal;
+        if (this.status >= 1) {
+          // TODO task is complete, delete task
+          return 1;
+        }
+        return res;
+      } else {
+        const res = creep.myMoveTo(structure);
+        return res;
+      }
+    }
+}
+
+class takeTask extends task {
+  constructor(priority, srcType, amount, target, aciton = this.defaultAction) {
+    this.priority = priority;
+    this.srcType = srcType;
+    this.progressTotal = amount;
+    /** @type {AnyStoreStructure} */
+    this.target = target;
+    this.aciton = aciton;
+    this.progress = 0;
+  }
+
+    /**
+     * creep will take $amount of $srcType to $target
+     * @param {Creep} creep the creep to perform task
+     * @returns {number} result of task:
+     * 1: complete
+     * 0: OK
+     * <0: one of the ERR_*
+     */
+    defaultAction(creep){
+      // pay structure
+      const structure = this.target;
+      if (creep.pos.isNearTo(structure)) {
+        const amountCarried = creep.store.getUsedCapacity(this.srcType);
+        const res = creep.withdraw(structure, this.srcType);
+        const amountWithdrawn = creep.store.getUsedCapacity(this.srcType) - amountCarried;
+        this.progress += amountWithdrawn;
+        this.status = this.progress / this.progressTotal;
+        if (this.status >= 1) {
+          // TODO task is complete, delete task
+          return 1;
+        }
+        return res;
+      } else {
+        const res = creep.myMoveTo(structure);
+        return res;
+      }
+    }
 }
